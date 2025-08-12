5889ae4de0896e4fce78b2943cd1046cb5256e3c
Spencer Balogh <spbalogh@gmail.com>
2025-08-11 19:29:36 -0500

    feature(nix module) - rework to utilize config
    Dropping ideas of backwards compatibility with system.autoUpgrade in
    favor of adopting more modern module conventions. Now writes a yaml
    config generated from `cfg.settings` to
    /etc/nixos-hydra-upgrade/config.yaml and documents that settings
    instead.
    
    Also adding environmentFile for keeping settings out of plain text
    configs and the nix store.

--- modules/nixos-hydra-upgrade/default.nix
+++ modules/nixos-hydra-upgrade/default.nix
@@ -7,91 +7,23 @@
 }: let
   cfg = config.system.autoUpgradeHydra;
   nixosHydraUpgradePackages = inputs.nixos-hydra-upgrade.packages.${pkgs.system};
+  settingsFormat = pkgs.formats.yaml {};
 in {
-  /*
-  Automatic system upgrades with nixos-hydra-upgrade.
-
-  A lot of this is a drop-in replacement for `system.autoUpgrade`, but a few options are
-  dropped and a few new are introduced:
-  - Channels are not supported.
-  - The flake option is replaced with a hydra job that discovers the flake.
-  - Delay options aren't implemented (not against supporting them, but I don't use them).
-  - rebootWindow isn't implemented (ditto)
-  - persistent isn't implemented (ditto)
-  */
   options = {
     system.autoUpgradeHydra = {
       enable = lib.mkEnableOption "Whether to perform system upgrades with nixos-hydra-upgrade.";
 
-      operation = lib.mkOption {
-        type = lib.types.enum [
-          "switch"
-          "boot"
-        ];
-        default = "switch";
-        description = lib.mdDoc "{command}`nixos-rebuild` operation to execute";
-      };
+      environmentFile = lib.mkOption {
+        type = lib.types.nullOr lib.types.path;
+        default = null;
+        description = ''
+          Environment file (see {manpage}`systemd.exec(5)`
+          "EnvironmentFile=" section for the syntax) to define service environment variables.
+          This option may be used to safely include secrets without exposure in the nix store.
 
-      host = lib.mkOption {
-        type = lib.types.str;
-        description = lib.mdDoc "system hostname";
-        default = config.networking.hostName;
-      };
-
-      hydra = lib.mkOption {
-        description = lib.mdDoc ''
-          Options to specify the hydra build.
+          See [usage](https://github.com/hyperparabolic/nixos-hydra-upgrade/blob/main/README.md#usage)
+          for environment variable details.
         '';
-        type = lib.types.submodule {
-          options = {
-            instance = lib.mkOption {
-              type = lib.types.str;
-              example = "https://hydra.oak.decent.id";
-              description = lib.mdDoc "hydra instance to query";
-            };
-            project = lib.mkOption {
-              type = lib.types.str;
-              description = lib.mdDoc "hydra project";
-            };
-            jobset = lib.mkOption {
-              type = lib.types.str;
-              description = lib.mdDoc "hydra jobset";
-            };
-            job = lib.mkOption {
-              type = lib.types.str;
-              description = lib.mdDoc "hydra job";
-            };
-          };
-        };
-      };
-
-      healthChecks = lib.mkOption {
-        description = lib.mdDoc ''
-          Options to specify health checks to perform before system upgrade.
-        '';
-        type = lib.types.submodule {
-          options = {
-            canaryHosts = lib.mkOption {
-              type = lib.types.listOf lib.types.str;
-              default = [];
-              description = lib.mdDoc ''
-                Hosts specified here are checked with a simple ICMP ping
-                healthcheck. If all hosts do not reply the upgrade will
-                be aborted. Probably does not properly escape every valid
-                url.
-              '';
-            };
-          };
-        };
-      };
-
-      flags = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [];
-        example = [
-          "--accept-flake-config"
-        ];
-        description = lib.mdDoc "Additional flags to pass to {command}`nixos-rebuild`";
       };
 
       dates = lib.mkOption {
@@ -106,61 +38,147 @@ in {
         '';
       };
 
-      allowReboot = lib.mkOption {
-        default = false;
-        type = lib.types.bool;
-        description = lib.mdDoc ''
-          Wether to reboot the system following a successful {command}`nixos-rebuild`.
+      settings = lib.mkOption {
+        description = ''
+          Configuration for nixos-hydra-upgrade, see [usage](https://github.com/hyperparabolic/nixos-hydra-upgrade/blob/main/README.md#usage)
+          for details on all supported values.
         '';
+        type = lib.types.submodule {
+          freeformType = settingsFormat.type;
+          options = {
+            debug = lib.mkOption {
+              type = lib.types.bool;
+              description = "enable debug logging";
+              default = false;
+            };
+            healthChecks = lib.mkOption {
+              description = ''
+                Options to specify health checks to perform before system upgrade.
+              '';
+              type = lib.types.submodule {
+                freeformType = settingsFormat.type;
+                options = {
+                  canaryHosts = lib.mkOption {
+                    type = lib.types.listOf lib.types.str;
+                    default = [];
+                    description = ''
+                      Hosts specified here are checked with a simple ICMP ping
+                      healthcheck. If all hosts do not reply the upgrade will
+                      be aborted. Probably does not properly escape every valid
+                      url.
+                    '';
+                  };
+                };
+              };
+            };
+            hydra = lib.mkOption {
+              description = ''
+                Options to specify the hydra build.
+              '';
+              type = lib.types.submodule {
+                freeformType = settingsFormat.type;
+                options = {
+                  instance = lib.mkOption {
+                    type = lib.types.str;
+                    example = "https://hydra.oak.decent.id";
+                    description = "hydra instance to query";
+                  };
+                  project = lib.mkOption {
+                    type = lib.types.str;
+                    description = "hydra project";
+                  };
+                  jobset = lib.mkOption {
+                    type = lib.types.str;
+                    description = "hydra jobset";
+                  };
+                  job = lib.mkOption {
+                    type = lib.types.str;
+                    description = "hydra job";
+                  };
+                };
+              };
+            };
+            nixos-rebuild = lib.mkOption {
+              description = ''
+                Options to pass to `nixos-rebuild`.
+              '';
+              type = lib.types.submodule {
+                freeformType = settingsFormat.type;
+                options = {
+                  operation = lib.mkOption {
+                    type = lib.types.enum [
+                      "switch"
+                      "boot"
+                    ];
+                    default = "boot";
+                    description = "{command}`nixos-rebuild` operation to execute";
+                  };
+                  host = lib.mkOption {
+                    type = lib.types.str;
+                    description = "system hostname";
+                    default = config.networking.hostName;
+                  };
+                  args = lib.mkOption {
+                    type = lib.types.listOf lib.types.str;
+                    default = [];
+                    example = [
+                      "--accept-flake-config"
+                    ];
+                    description = "Additional flags to pass to {command}`nixos-rebuild`";
+                  };
+                };
+              };
+            };
+            reboot = lib.mkOption {
+              default = false;
+              type = lib.types.bool;
+              description = ''
+                Wether to reboot the system following a successful {command}`nixos-rebuild`.
+              '';
+            };
+          };
+        };
+        default = {};
       };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.services.nixos-hydra-upgrade = {
-      description = "NixOS Upgrade with hydra build validation and health check support.";
-
-      restartIfChanged = false;
-      unitConfig.X-StopOnRemoval = false;
-      serviceConfig.Type = "oneshot";
-
-      environment =
-        config.nix.envVars
-        // {
-          inherit (config.environment.sessionVariables) NIX_PATH;
-          HOME = "/root";
-        }
-        // config.networking.proxy.envVars;
-
-      path = [
-        config.nix.package
-        config.system.build.nixos-rebuild
-      ];
-
-      script = let
-        nixos-hydra-upgrade = "${lib.getExe nixosHydraUpgradePackages.default}";
-        reboot-arg = lib.optionalString cfg.allowReboot "--reboot";
-        canary-args =
-          lib.optionalString
-          (builtins.length cfg.healthChecks.canaryHosts > 0)
-          "${(lib.strings.concatMapStringsSep " " (x: "--canary=\"" + x + "\"") cfg.healthChecks.canaryHosts)}";
-        nixos-rebuild-args =
-          lib.optionalString
-          (builtins.length cfg.flags > 0)
-          "${(lib.strings.concatMapStringsSep " " (x: "--passthru-args=\"" + x + "\"") cfg.flags)}";
-      in ''
-        ${nixos-hydra-upgrade} ${cfg.operation} \
-          --host="${cfg.host}" \
-          --instance="${cfg.hydra.instance}" \
-          --project="${cfg.hydra.project}" \
-          --jobset="${cfg.hydra.jobset}" \
-          --job="${cfg.hydra.job}" ${canary-args} ${nixos-rebuild-args} ${reboot-arg}
-      '';
-
-      startAt = cfg.dates;
-
-      after = ["network-online.target"];
-      wants = ["network-online.target"];
+    environment.etc."nixos-hydra-upgrade" = {
+      mode = "0440";
+      source = settingsFormat.generate "nixos-hydra-upgrade.yaml" cfg.settings;
+      target = "nixos-hydra-upgrade/config.yaml";
     };
+    systemd.services.nixos-hydra-upgrade =
+      {
+        description = "NixOS Upgrade with hydra build validation and health check support.";
+
+        restartIfChanged = false;
+        unitConfig.X-StopOnRemoval = false;
+        serviceConfig.Type = "oneshot";
+
+        environment =
+          config.nix.envVars
+          // {
+            inherit (config.environment.sessionVariables) NIX_PATH;
+            HOME = "/root";
+          }
+          // config.networking.proxy.envVars;
+
+        path = [
+          config.nix.package
+          config.system.build.nixos-rebuild
+        ];
+
+        script = "${lib.getExe nixosHydraUpgradePackages.default}";
+
+        startAt = cfg.dates;
+
+        after = ["network-online.target"];
+        wants = ["network-online.target"];
+      }
+      // lib.optionalAttrs (cfg.environmentFile != null) {
+        EnvironmentFile = cfg.environmentFile;
+      };
   };
 }
